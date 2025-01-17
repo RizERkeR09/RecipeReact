5da59bf83da5a7574aff22b93a5783d69f0042cc
RizERkeR09 <omstha71@gmail.com>
2025-01-17 22:23:49 +0545

    Add files via upload

---
+++ Recipy/Body.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+import InList from "./InList"
+import Recipe from "./Recipe"
+
+export default function Body() {
+    const [ingredientList, setIngredientList] = useState(["oregano", "dough", "Spices", "tomato"])
+
+
+
+    const ingredient = ingredientList.map(item => (
+        <li key={item}>{item}</li>
+    ))
+
+    const [recipeShown, setRecipeShown] = useState(false)
+
+    function getRecipe() {
+        setRecipeShown(prevShown => !prevShown)
+    }
+
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget) //creating object of input form form input
+        const newIngredient = formData.get("ingredient")
+        setIngredientList(prevIngredient => [...prevIngredient, newIngredient])
+
+        event.target.reset();
+
+    }
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit} className="form">
+                <input
+                    type="text"
+                    placeholder="e.g. Oregano"
+                    aria-label=" Add ingrediants"
+                    name="ingredient" />
+                <button>Add Ingredient</button>
+            </form>
+
+            {ingredientList.length > 0 && <InList 
+            ingredient={ingredient}
+            getRecipe= {getRecipe}
+            />}
+            {recipeShown && <Recipe />}
+        </main>
+    )
+}
---
+++ Recipy/Recipe.jsx
@@ -0,0 +1,14 @@
+export default function Recipe(){
+    return(
+        <section>
+                <h1>Here is your recipe</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+                    Facilis blanditiis repellendus omnis ut officia minus beatae <br />
+                    voluptate corrupti? Molestias excepturi quidem, animi dolorem <br />
+                    omnis veniam ea quia fugiat labore qui. Quis aperiam distinctio <br />
+                    placeat enim nobis fugiat dignissimos sed, obcaecati eaque minima magni <br />
+                    ullam ratione, hic pariatur? Repellat, reiciendis. Aliquid explicabo veritatis <br />
+                    nulla asperiores illo.</p>
+            </section>
+    )
+}
